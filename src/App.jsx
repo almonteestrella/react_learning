772d4c545aae79e928fd3ee2facdcc8c16a70b31
Juan Almonte <almonteestrella@gmail.com>
2023-07-07 10:58:33 -0400

    tailwind implementation

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import Employee from './components/List';
 
 function App() {
     const showConditionally = true;
-    const [role, setRole] = useState('student');
+    const [role, setRole] = useState('student'); //way to change values on the UI.
 
     return (
         <>
             {showConditionally ? (
-                <div>
+                <div className=''>
                     <input
                         type='text'
                         onChange={(e) => {
@@ -16,9 +16,22 @@ function App() {
                             setRole(e.target.value);
                         }}
                     />
-                    <Employee name='juan' />
-                    <Employee name='maria' role='dentist' />
-                    <Employee name='daniela' role={role} />
+                    <div className='flex flex-wrap justify-center'>
+                        <Employee
+                            name='juan'
+                            img='https://images.pexels.com/photos/895259/pexels-photo-895259.jpeg?auto=compress&cs=tinysrgb&w=600'
+                        />
+                        <Employee
+                            name='maria'
+                            role='dentist'
+                            img='https://images.pexels.com/photos/2607544/pexels-photo-2607544.jpeg?auto=compress&cs=tinysrgb&w=600'
+                        />
+                        <Employee
+                            name='daniela'
+                            role={role}
+                            img='https://images.pexels.com/photos/733835/pexels-photo-733835.jpeg?auto=compress&cs=tinysrgb&w=600'
+                        />
+                    </div>
                 </div>
             ) : (
                 <h1>I can not see the list</h1>
