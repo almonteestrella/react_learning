85d9a12c738d3c11c0f590045ce53f80899eb0f8
Juan Almonte <almonteestrella@gmail.com>
2023-07-08 18:46:38 -0400

    form added

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,3 +1,5 @@
+import EditEmployee from './EditEmployeeModal';
+
 /* eslint react/prop-types: 0 */
 const Employee = ({ name, role, img }) => {
     return (
@@ -14,9 +16,7 @@ const Employee = ({ name, role, img }) => {
                         {role ? role : 'no role'}
                     </p>
                 </div>
-                <button className='px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2'>
-                    Message
-                </button>
+                <EditEmployee />
             </div>
         </div>
     );
