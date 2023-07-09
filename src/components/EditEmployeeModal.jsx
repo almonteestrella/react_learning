c7e9b04d5c28334dd7a2e58fc21d2adbcbb8a90a
Juan Almonte <almonteestrella@gmail.com>
2023-07-08 20:44:24 -0400

    form integration

--- src/components/EditEmployeeModal.jsx
+++ src/components/EditEmployeeModal.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
+
 import Modal from 'react-bootstrap/Modal';
 
 function EditEmployee() {
@@ -19,18 +19,61 @@ function EditEmployee() {
 
             <Modal show={show} onHide={handleClose}>
                 <Modal.Header closeButton>
-                    <Modal.Title>Modal heading</Modal.Title>
+                    <Modal.Title>Update Employee</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    Woohoo, you are reading this text in a modal!
+                    <form className='w-full max-w-sm' id='form'>
+                        <div className='md:flex md:items-center mb-6'>
+                            <div className='md:w-1/3'>
+                                <label
+                                    className='block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4'
+                                    htmlFor='name'
+                                >
+                                    Full Name
+                                </label>
+                            </div>
+                            <div className='md:w-2/3'>
+                                <input
+                                    className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500'
+                                    id='name'
+                                    type='text'
+                                    value='Jane Doe'
+                                />
+                            </div>
+                        </div>
+                        <div className='md:flex md:items-center mb-6'>
+                            <div className='md:w-1/3'>
+                                <label
+                                    className='block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4'
+                                    htmlFor='role'
+                                >
+                                    Role
+                                </label>
+                            </div>
+                            <div className='md:w-2/3'>
+                                <input
+                                    className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500'
+                                    id='role'
+                                    type='text'
+                                    value='Jane Doe'
+                                />
+                            </div>
+                        </div>
+                    </form>
                 </Modal.Body>
                 <Modal.Footer>
-                    <Button variant='secondary' onClick={handleClose}>
+                    <button
+                        className='bg-slate-400 hover:bg-slate-500 text-white font-bold py-2 px-4 rounded-full'
+                        onClick={handleClose}
+                    >
                         Close
-                    </Button>
-                    <Button variant='primary' onClick={handleClose}>
-                        Save Changes
-                    </Button>
+                    </button>
+                    <button
+                        form='form'
+                        className='bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded-full'
+                    >
+                        Update
+                    </button>
                 </Modal.Footer>
             </Modal>
         </>
