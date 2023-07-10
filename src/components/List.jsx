7d439d8caddb20ec15fcf819aed9952443ee46dc
Juan Almonte <almonteestrella@gmail.com>
2023-07-09 20:55:27 -0400

    new update from the form

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,22 +1,22 @@
 import EditEmployee from './EditEmployeeModal';
 
 /* eslint react/prop-types: 0 */
-const Employee = ({ name, role, img }) => {
+const Employee = (props) => {
     return (
-        <div className='m-2 py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6'>
+        <div className='min-w-[350px] max-w-[350px] m-2 py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6'>
             <img
                 className='object-cover rounded-full h-[100px] w-[100px] block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0'
-                src={img}
+                src={props.img}
                 alt="Woman's Face"
             />
             <div className='text-center space-y-2 sm:text-left'>
                 <div className='space-y-0.5'>
                     <p className='text-lg text-black font-semibold'>{name}</p>
                     <p className='text-slate-500 font-medium'>
-                        {role ? role : 'no role'}
+                        {props.role ? props.role : 'no role'}
                     </p>
                 </div>
-                <EditEmployee />
+                <EditEmployee name={props.name} role={props.role} />
             </div>
         </div>
     );
