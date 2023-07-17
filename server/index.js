a0eed2c78e289126e583e1e2fdcbd9b8c32fa771
Juan Almonte <almonteestrella@gmail.com>
2023-07-16 20:37:50 -0400

    update from the server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
-//basic express server
-const express = require('express');
+import express from 'express';
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); // To parse the incoming requests with JSON payloads
 
 app.get('/', (req, res) => {
     res.status(200).send('home page');
@@ -12,6 +13,6 @@ app.all('*', (req, res) => {
     res.status(404).send('<h1>Resource not found!</h1>');
 });
 
-const port = 3000;
+const port = 5000;
 
 app.listen(port, () => console.log(`server running on port ${port}`));
